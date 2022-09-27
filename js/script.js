4dfb23ac7a13ca2637d263a7d398b806f66829a0
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-27 18:37:53 +0300

    // DomStrings const { body } = document; const brush = document.getElementById('brush'); const bucket = document.getElementById('bucket'); const eraser = document.getElementById('eraser'); const brushSlider = document.getElementById('brush-slider'); const brushSize = document.getElementById('brush-size'); const clearCanvasBtn = document.getElementById('clear-canvas'); const saveStorageBtn = document.getElementById('save-storage'); const loadStorageBtn = document.getElementById('load-storage'); const clearStorageBtn = document.getElementById('clear-storage'); const downloadBtn = document.getElementById('download'); const undoBtn = document.getElementById('undo'); const redoBtn = document.getElementById('redo'); const colorSelector = document.getElementById('bucket-color');

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// DomStrings
+const { body } = document;
+const brush = document.getElementById('brush');
+const bucket = document.getElementById('bucket');
+const eraser = document.getElementById('eraser');
+const brushSlider = document.getElementById('brush-slider');
+const brushSize = document.getElementById('brush-size');
+const clearCanvasBtn = document.getElementById('clear-canvas');
+const saveStorageBtn = document.getElementById('save-storage');
+const loadStorageBtn = document.getElementById('load-storage');
+const clearStorageBtn = document.getElementById('clear-storage');
+const downloadBtn = document.getElementById('download');
+const undoBtn = document.getElementById('undo');
+const redoBtn = document.getElementById('redo');
+const colorSelector = document.getElementById('bucket-color');
