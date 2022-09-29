15ddb3b43a3752a86c44d1404f32a5ad7e2988d7
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-29 06:40:08 +0300

    var jsc = {
    	register : function () {
    		jsc.attachDOMReadyEvent(jsc.init);
    		jsc.attachEvent(document, 'mousedown', jsc.onDocumentMouseDown);
    		jsc.attachEvent(document, 'touchstart', jsc.onDocumentTouchStart);
    		jsc.attachEvent(window, 'resize', jsc.onWindowResize);
    	},

--- js/jscolor.js
+++ js/jscolor.js
@@ -2,3 +2,13 @@
 
 
 if (!window.jscolor) { window.jscolor = (function () {
+
+    var jsc = {
+
+
+        register : function () {
+            jsc.attachDOMReadyEvent(jsc.init);
+            jsc.attachEvent(document, 'mousedown', jsc.onDocumentMouseDown);
+            jsc.attachEvent(document, 'touchstart', jsc.onDocumentTouchStart);
+            jsc.attachEvent(window, 'resize', jsc.onWindowResize);
+        },
