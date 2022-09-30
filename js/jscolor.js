ba98cad9bcc16bb40c4ab5f0ada0ab676fb25542
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-30 20:40:13 +0300

    BoxShadow : (function () { 		var BoxShadow = function (hShadow, vShadow, blur, spread, color, inset) { 			this.hShadow = hShadow; 			this.vShadow = vShadow; 			this.blur = blur; 			this.spread = spread; 			this.color = color; 			this.inset = !!inset; 		};
    		BoxShadow.prototype.toString = function () {
    			var vals = [
    				Math.round(this.hShadow) + 'px',
    				Math.round(this.vShadow) + 'px',
    				Math.round(this.blur) + 'px',
    				Math.round(this.spread) + 'px',
    				this.color
    			];
    			if (this.inset) {
    				vals.push('inset');
    			}
    			return vals.join(' ');
    		};
    
    		return BoxShadow;
    	})(),

--- js/jscolor.js
+++ js/jscolor.js
@@ -880,3 +880,31 @@ if (!window.jscolor) { window.jscolor = (function () {
 	leaveStyle : 1<<1,
 	leavePad : 1<<2,
 	leaveSld : 1<<3,
+
+
+    BoxShadow : (function () {
+		var BoxShadow = function (hShadow, vShadow, blur, spread, color, inset) {
+			this.hShadow = hShadow;
+			this.vShadow = vShadow;
+			this.blur = blur;
+			this.spread = spread;
+			this.color = color;
+			this.inset = !!inset;
+		};
+
+		BoxShadow.prototype.toString = function () {
+			var vals = [
+				Math.round(this.hShadow) + 'px',
+				Math.round(this.vShadow) + 'px',
+				Math.round(this.blur) + 'px',
+				Math.round(this.spread) + 'px',
+				this.color
+			];
+			if (this.inset) {
+				vals.push('inset');
+			}
+			return vals.join(' ');
+		};
+
+		return BoxShadow;
+	})(),
