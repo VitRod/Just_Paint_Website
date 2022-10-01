ca4a435bb0f47f113c24027411bcdd788ebd329d
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-01 11:59:29 +0300

    function redrawPad () { 			// redraw the pad pointer 			switch (jsc.getPadYComponent(THIS)) { 			case 's': var yComponent = 1; break; 			case 'v': var yComponent = 2; break; 			} 			var x = Math.round((THIS.hsv[0] / 360) * (THIS.width - 1)); 			var y = Math.round((1 - THIS.hsv[yComponent] / 100) * (THIS.height - 1)); 			var crossOuterSize = (2 * THIS.pointerBorderWidth + THIS.pointerThickness + 2 * THIS.crossSize); 			var ofs = -Math.floor(crossOuterSize / 2); 			jsc.picker.cross.style.left = (x + ofs) + 'px'; 			jsc.picker.cross.style.top = (y + ofs) + 'px';
    			// redraw the slider
    			switch (jsc.getSliderComponent(THIS)) {
    			case 's':
    				var rgb1 = HSV_RGB(THIS.hsv[0], 100, THIS.hsv[2]);
    				var rgb2 = HSV_RGB(THIS.hsv[0], 0, THIS.hsv[2]);
    				var color1 = 'rgb(' +
    					Math.round(rgb1[0]) + ',' +
    					Math.round(rgb1[1]) + ',' +
    					Math.round(rgb1[2]) + ')';
    				var color2 = 'rgb(' +
    					Math.round(rgb2[0]) + ',' +
    					Math.round(rgb2[1]) + ',' +
    					Math.round(rgb2[2]) + ')';
    				jsc.picker.sldGrad.draw(THIS.sliderSize, THIS.height, color1, color2);
    				break;
    			case 'v':
    				var rgb = HSV_RGB(THIS.hsv[0], THIS.hsv[1], 100);
    				var color1 = 'rgb(' +
    					Math.round(rgb[0]) + ',' +
    					Math.round(rgb[1]) + ',' +
    					Math.round(rgb[2]) + ')';
    				var color2 = '#000';
    				jsc.picker.sldGrad.draw(THIS.sliderSize, THIS.height, color1, color2);
    				break;
    			}
    		}

--- js/jscolor.js
+++ js/jscolor.js
@@ -1648,6 +1648,47 @@ if (!window.jscolor) { window.jscolor = (function () {
 	}
 
 
+	function redrawPad () {
+		// redraw the pad pointer
+		switch (jsc.getPadYComponent(THIS)) {
+		case 's': var yComponent = 1; break;
+		case 'v': var yComponent = 2; break;
+		}
+		var x = Math.round((THIS.hsv[0] / 360) * (THIS.width - 1));
+		var y = Math.round((1 - THIS.hsv[yComponent] / 100) * (THIS.height - 1));
+		var crossOuterSize = (2 * THIS.pointerBorderWidth + THIS.pointerThickness + 2 * THIS.crossSize);
+		var ofs = -Math.floor(crossOuterSize / 2);
+		jsc.picker.cross.style.left = (x + ofs) + 'px';
+		jsc.picker.cross.style.top = (y + ofs) + 'px';
+
+		// redraw the slider
+		switch (jsc.getSliderComponent(THIS)) {
+		case 's':
+			var rgb1 = HSV_RGB(THIS.hsv[0], 100, THIS.hsv[2]);
+			var rgb2 = HSV_RGB(THIS.hsv[0], 0, THIS.hsv[2]);
+			var color1 = 'rgb(' +
+				Math.round(rgb1[0]) + ',' +
+				Math.round(rgb1[1]) + ',' +
+				Math.round(rgb1[2]) + ')';
+			var color2 = 'rgb(' +
+				Math.round(rgb2[0]) + ',' +
+				Math.round(rgb2[1]) + ',' +
+				Math.round(rgb2[2]) + ')';
+			jsc.picker.sldGrad.draw(THIS.sliderSize, THIS.height, color1, color2);
+			break;
+		case 'v':
+			var rgb = HSV_RGB(THIS.hsv[0], THIS.hsv[1], 100);
+			var color1 = 'rgb(' +
+				Math.round(rgb[0]) + ',' +
+				Math.round(rgb[1]) + ',' +
+				Math.round(rgb[2]) + ')';
+			var color2 = '#000';
+			jsc.picker.sldGrad.draw(THIS.sliderSize, THIS.height, color1, color2);
+			break;
+		}
+	}
+
+
 
 
 
