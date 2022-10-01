7f6781c803e4d263513d8bbb6ac0c52f48ef2397
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-01 12:00:13 +0300

    function redrawSld () { 			var sldComponent = jsc.getSliderComponent(THIS); 			if (sldComponent) { 				// redraw the slider pointer 				switch (sldComponent) { 				case 's': var yComponent = 1; break; 				case 'v': var yComponent = 2; break; 				} 				var y = Math.round((1 - THIS.hsv[yComponent] / 100) * (THIS.height - 1)); 				jsc.picker.sldPtrOB.style.top = (y - (2 * THIS.pointerBorderWidth + THIS.pointerThickness) - Math.floor(sliderPtrSpace / 2)) + 'px'; 			} 		}
    		function isPickerOwner () {
    			return jsc.picker && jsc.picker.owner === THIS;
    		}
    
    		function blurValue () {
    			THIS.importColor();
    		}

--- js/jscolor.js
+++ js/jscolor.js
@@ -1689,6 +1689,31 @@ if (!window.jscolor) { window.jscolor = (function () {
 	}
 
 
+	function redrawSld () {
+		var sldComponent = jsc.getSliderComponent(THIS);
+		if (sldComponent) {
+			// redraw the slider pointer
+			switch (sldComponent) {
+			case 's': var yComponent = 1; break;
+			case 'v': var yComponent = 2; break;
+			}
+			var y = Math.round((1 - THIS.hsv[yComponent] / 100) * (THIS.height - 1));
+			jsc.picker.sldPtrOB.style.top = (y - (2 * THIS.pointerBorderWidth + THIS.pointerThickness) - Math.floor(sliderPtrSpace / 2)) + 'px';
+		}
+	}
+
+
+	function isPickerOwner () {
+		return jsc.picker && jsc.picker.owner === THIS;
+	}
+
+
+	function blurValue () {
+		THIS.importColor();
+	}
+
+
+
 
 
 
